65415bf800d686ff3586d8c123dd19fe64a26a1b
EGollahon <horselover@stellarwerx.com>
2022-10-01 02:06:57 -0400

    added seasons changing and tiles changing

--- Assets/Scripts/SeasonManager.cs
+++ Assets/Scripts/SeasonManager.cs
@@ -1,16 +1,28 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Tilemaps;
 
 public class SeasonManager : MonoBehaviour
 {
     int currentSeasonIndex = 0;
     string[] seasonArray = {"Winter", "Spring", "Summer", "Autumn"};
     float seasonTimer = 10.0f;
+    public GameObject tilemapObject;
+    Tilemap tilemap;
+
+    Object[] winterTileArray;
+    Object[] springTileArray;
+    Object[] summerTileArray;
+    Object[] autumnTileArray;
 
     void Start()
     {
-        Debug.Log("season start");
+        tilemap = tilemapObject.GetComponent<Tilemap>();
+        winterTileArray = Resources.LoadAll("Winter Tiles", typeof(TileBase));
+        springTileArray = Resources.LoadAll("Spring Tiles", typeof(TileBase));
+        summerTileArray = Resources.LoadAll("Summer Tiles", typeof(TileBase));
+        autumnTileArray = Resources.LoadAll("Autumn Tiles", typeof(TileBase));
     }
 
     void Update()
@@ -24,7 +36,40 @@ public class SeasonManager : MonoBehaviour
                     currentSeasonIndex = currentSeasonIndex + 1;
                 }
                 seasonTimer = 10.0f;
-                Debug.Log(seasonArray[currentSeasonIndex]);
+                ChangeSeasonTiles();
+            }
+        }
+    }
+
+    void ChangeSeasonTiles()
+    {
+        if (seasonArray[currentSeasonIndex] == "Winter") {
+            foreach (TileBase tile in winterTileArray)
+            {
+                tilemap.SwapTile(System.Array.Find(springTileArray, t => t.name == tile.name) as TileBase, tile);
+                tilemap.SwapTile(System.Array.Find(summerTileArray, t => t.name == tile.name) as TileBase, tile);
+                tilemap.SwapTile(System.Array.Find(autumnTileArray, t => t.name == tile.name) as TileBase, tile);
+            }
+        } else if (seasonArray[currentSeasonIndex] == "Spring") {
+            foreach (TileBase tile in springTileArray)
+            {
+                tilemap.SwapTile(System.Array.Find(summerTileArray, t => t.name == tile.name) as TileBase, tile);
+                tilemap.SwapTile(System.Array.Find(autumnTileArray, t => t.name == tile.name) as TileBase, tile);
+                tilemap.SwapTile(System.Array.Find(winterTileArray, t => t.name == tile.name) as TileBase, tile);
+            }
+        } else if (seasonArray[currentSeasonIndex] == "Summer") {
+            foreach (TileBase tile in summerTileArray)
+            {
+                tilemap.SwapTile(System.Array.Find(autumnTileArray, t => t.name == tile.name) as TileBase, tile);
+                tilemap.SwapTile(System.Array.Find(winterTileArray, t => t.name == tile.name) as TileBase, tile);
+                tilemap.SwapTile(System.Array.Find(springTileArray, t => t.name == tile.name) as TileBase, tile);
+            }
+        } else if (seasonArray[currentSeasonIndex] == "Autumn") {
+            foreach (TileBase tile in autumnTileArray)
+            {
+                tilemap.SwapTile(System.Array.Find(winterTileArray, t => t.name == tile.name) as TileBase, tile);
+                tilemap.SwapTile(System.Array.Find(springTileArray, t => t.name == tile.name) as TileBase, tile);
+                tilemap.SwapTile(System.Array.Find(summerTileArray, t => t.name == tile.name) as TileBase, tile);
             }
         }
     }
